68a3b2cb77b676851e8bfd0fd3d058e036d639d5
maria <128487121+mariaclaraaguiar123@users.noreply.github.com>
2024-04-23 14:09:22 -0300

    ajustando pronomes

--- main.js
+++ main.js
@@ -9,4 +9,27 @@ for(let i=0;i <botoes.length;i++){
 
         botoes[i].classList.add("ativo");
     }
+}const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+contadores[0].textContent = calculaTempo(tempoObjetivo1); //Antes
+
+contadores[0].textContent = calculaTempo(tempos[0]); // Depois
+const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
 }
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
+
